d52922eae5cae997264925d035c0f99789ec8d66
girishg <girishrathode.rathode@gmail.com>
2021-05-28 11:57:25 +0530

    * UnitTest1.cs: * XPATH_Contains.cs: * My_KidsBank_SpecFlow.csproj:
    * Banker_Project.sln: 28 - 05 -2021

---
+++ My_KidsBank/Locaters/XPATH_Contains.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace My_KidsBank.Locaters
+{
+    public class XPATH_Contains
+    {
+        [Test]
+        public void test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/register.htm");
+
+            driver.FindElement(By.XPath("//*[@id='customer.firstNames' or @name='customer.firstName']")).SendKeys("Devil121");
+            driver.FindElement(By.XPath("//*[@id='customer.lastNames' and @name='customer.lastName']")).SendKeys("DVampire");
+
+        }
+    }
+}
